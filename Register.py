dbe4cb4ab473293549d6079b5e21eed56e02ddfd
Triz-tan <72549251+Triz-tan@users.noreply.github.com>
2023-05-18 12:53:34 +0800

    update

--- Register.py
+++ Register.py
@@ -3,6 +3,9 @@ import style_register as style
 import cv2
 from PIL import Image, ImageTk
 from Home import App
+import os
+import tkinter
+import tkinter.messagebox
 class register_window(ctk.CTkToplevel):
     
     def __init__(self, master):
@@ -78,18 +81,46 @@ class register_window(ctk.CTkToplevel):
         self._label.imgtk = imgtk
         self._label.configure(image=imgtk)
         self._label.after(20, self.process_webcam)
-      
+    
+
+    def capture_func_btn(self):
+        
+        
+        user_id_value = self.user_entry.get() 
+
+        ret, frame = self.capture.read()
+        if ret:
+            if not os.path.exists('test_images'):
+                os.makedirs('test_images')
+            path_image = 'test_images/{}.jpg'.format(user_id_value) 
+            cv2.imwrite(path_image, frame)
+
+            #Create a new window for registration of the images
+            self.photo_register = ctk.CTkToplevel()
+            self.photo_register.geometry("1024x600")
+            self.photo_register.title("Photo Registration")
+            self.photo_register.attributes('-topmost', True)
+            self.attributes('-topmost', False)
+
+            self.photo_register_label = ctk.CTkLabel(self.photo_register, text="Photo Registration", font=('Poppins', 24), bg_color="#242424")
+            self.photo_register_label.place(x=28, y=21.5)
+
+            self.frame_photo = ctk.CTkFrame(self.photo_register, width=306, height=480, corner_radius=15)
+            self.frame_photo.place(x=28, y=90.8)
+
+            self.question_label = ctk.CTkLabel(self.photo_register, text="Is this photo okay to you", font=('Poppins', 20), bg_color="#2B2B2B")
+            self.question_label.place(x=60.9, y=114.8)
+
+            
+
+            self.photo_register.mainloop()
+
+        print("Capture function button just clicked")
     
 
 
 
 
-    def home_func_btn(self):
-        self.capture.release()
-        self.destroy()
-        App
-    
-        print("Home button just clicked")
 
     def face_func_btn(self):
         print("Face Recognize button just clicked")
@@ -97,9 +128,14 @@ class register_window(ctk.CTkToplevel):
     def about_func_btn(self):
         print("About us button just clicked")
 
-    def capture_func_btn(self):
-        print("Capture function button just clicked")
+    
 
+    
+    def home_func_btn(self):
+        self.capture.release()
+        self.destroy()
+        App
+        print("Home button just clicked")
 
 if __name__ == "__main__":
     root = ctk.CTk()
