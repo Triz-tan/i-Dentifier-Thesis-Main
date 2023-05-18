5608891087cec6cf3b763433e1f6044452ca7408
Triz-tan <72549251+Triz-tan@users.noreply.github.com>
2023-05-18 10:01:04 +0800

    update

--- style_register.py
+++ style_register.py
@@ -1,8 +1,9 @@
 import customtkinter as ctk
+import tkinter as tk
 
-def home_style(master, text, command):
+def btn_style(master, text, command):
     main_font = ctk.CTkFont(family="Poppins", size=14)
-    home_button_style = ctk.CTkButton(
+    btn = ctk.CTkButton(
                         master,
                         text= text,
                         command=command,
@@ -18,4 +19,46 @@ def home_style(master, text, command):
                         bg_color="transparent",
                         fg_color= "#403B3B"
                         )
-    return home_button_style
+    return btn
+
+def capture_btn_style(master, text, command):
+    main_font = ctk.CTkFont(family="Poppins", size=14)
+    btn = ctk.CTkButton(
+                        master,
+                        text= text,
+                        command=command,
+                        font= main_font,
+                        text_color="white",
+                        hover= True,
+                        hover_color= "green",
+                        height= 55,
+                        width= 132,
+                        border_width=1,
+                        corner_radius=10,
+                        border_color= "black",
+                        bg_color="#2B2B2B",
+                        fg_color= "#403B3B"
+                        )
+    return btn
+
+def entry_style(master, placeholder_text):
+    entry_name = ctk.CTkEntry(
+                        master,
+                        placeholder_text = placeholder_text,
+                        placeholder_text_color="Gray",
+                        font=('rockwell', 12),
+                        text_color="black",
+                        width=278,
+                        height=43,
+                        border_width=2,
+                        border_color= "black",
+                        bg_color="#262626",
+                        fg_color= "#EEEEEE",
+                        corner_radius=10
+                        )
+    return entry_name
+
+def webcam_style_label(window):
+    label = tk.Label(window)
+    label.grid(row=0, column=0)
+    return label
